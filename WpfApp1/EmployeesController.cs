343383e8d0a13bc465f692d354682c709549307a
תקשוב אשדוד <Owner@LAB28.MEKIF11.LOCAL>
2019-11-24 14:59:16 +0200

    Initial commit

---
+++ WpfApp1/EmployeesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class EmployeesController
+    {
+        Model model = new Model();
+
+        public List<Employees> GetEmployees()
+        {
+            return model.GetEmployees();
+        }
+    }
+}
---
+++ WpfApp1/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Model
+    {
+        dbEntities db = new dbEntities();
+
+        public List<Employees> GetEmployees()
+        {
+            return db.Employees.ToList();
+        }
+    }
+}
