57fdbe507489886c1071a4d3a9497191a9f46974
תקשוב אשדוד <Owner@LAB27.MEKIF11.LOCAL>
2020-01-26 15:00:00 +0200

    commit

--- WpfApp1/Model1.Designer.cs
+++ WpfApp1/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Owner\source\repos\WpfApp1\WpfApp1\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Owner\Desktop\final-project\WpfApp1\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
