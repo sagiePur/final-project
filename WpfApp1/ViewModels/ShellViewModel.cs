446bd149c7a620cfbde226c1394da976b4726002
Owner <Owner@LAB39>
2020-05-13 12:02:29 +0300

    push

---
+++ WpfApp1/ViewModels/ShellViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.ViewModels
+{
+    class ShellViewModel : Conductor<object>
+    {
+        public Dictionary<string, Screen> ViewsDict { get; set; } = new Dictionary<string, Screen>
+        {
+        };
+
+        public void ActivateView(string viewModelName)
+        {
+            DisplayName = $"Shop {viewModelName}";
+
+            ActivateItem(ViewsDict[viewModelName]);
+        }
+    }
+}
